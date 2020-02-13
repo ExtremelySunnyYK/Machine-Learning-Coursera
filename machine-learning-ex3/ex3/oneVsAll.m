1e375d6e0ce077548d1234896ab8047081c0cf0f
Chia Yong Kang <46377366+ExtremelySunnyYK@users.noreply.github.com>
2020-02-13 13:09:53 +0800

    oneVsAll

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -1,7 +1,5 @@
 function [all_theta] = oneVsAll(X, y, num_labels, lambda)
-%ONEVSALL trains multiple logistic regression classifiers and returns all
-%the classifiers in a matrix all_theta, where the i-th row of all_theta 
-%corresponds to the classifier for label i
+
 %   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
 %   logistic regression classifiers and returns each of these classifiers
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
@@ -9,6 +7,7 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 
 % Some useful variables
 m = size(X, 1);
+% Query only the length of the second dimension of A.
 n = size(X, 2);
 
 % You need to return the following variables correctly 
@@ -47,13 +46,18 @@ X = [ones(m, 1) X];
 %     [theta] = ...
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
-%
 
 
+for c = 1:num_labels
+  initial_theta = zeros(n + 1, 1);
+  
+  % Optimize
+  options = optimset('GradObj', 'on', 'MaxIter', 50);
+  [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)),initial_theta, options);
+   all_theta(c, :) = theta';
 
-
-
-
+  
+endfor
 
 
 
